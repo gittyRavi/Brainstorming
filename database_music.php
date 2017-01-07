f461d35c786742b1d75117998bb62dc2dcb77c5c
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:27:43 +0530

    Create database_music.php

---
+++ database_music.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Videos/Songs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+	.circle
+	{
+		height:2em;
+		width:2em;
+		border-radius:100%;
+		background-color:lightblue;
+		position:absolute;
+		margin:auto;
+	}
+	p.circle
+	{
+		background-color:orange;
+	}
+	img	
+	{
+		border-radius:10%;
+	}
+	.nav.navbar-nav.navbar-right li a
+	{
+		color: rgba(255,255,255,0.8);
+		
+	}
+	.nav.navbar-nav.navbar-right ul.dropdown-menu li a
+	{
+		color: grey;
+		opacity:1;
+	}
+	.nav.navbar-nav.navbar-right li.active a
+	{
+		color: white;
+		opacity:1;
+	}
+	pre
+	{
+		background-color:black;
+		color:yellow;
+		border:0;
+		opacity:0.8;
+	}
+	pre>b>a
+	{
+		color:yellow;
+	}
+	pre>div.circle
+	{
+		background-color:red;
+	}
+  </style>
+</head>
+<body style="background-color:black;">
+
+<nav class="navbar navbar-inverse">
+
+<button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target=".navbar-collapse">
+<span class="sr-only">Toggle Navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+
+<a class="navbar-brand active" href="database_music.php" style="color:orange;font-family:jokerman;background-color:#191A1C;">Database Songs</a>
+
+<div class="collapse navbar-collapse">
+<ul class="nav navbar-nav navbar-right">
+
+<li><a href="main_info.php"><span class="glyphicon glyphicon-user"></span> ~About My Works</a></li>
+
+<li>
+<a href="index.html">Home</a>
+</li>
+
+<li>
+<a href="menu.php">Menu</a>
+</li>
+
+<li class="dropdown">
+<a href="archive.php" class="dropdown-toggle" data-toggle="dropdown">Archive<b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="archive.php">Archive:Main</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Learn</li>
+<li><a href="learncplusplus.php">C++</a></li>
+<li><a href="learnjava.php">Java</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Extras</li>
+<li><a href="ecomenu.php">Eco</a></li>
+<li><a href="search_video.html">Music</a></li>
+</ul>
+</li>
+
+<li>
+<a href="contacts.php">Contact</a>
+</li>
+
+</ul>
+</div>
+
+</nav>
+
+<br/>
+  <div class="container text-center" style="background-color:#3E423E;border-radius:20px;">     
+    <p style="color:violet;position:center center;margin-top:0.8em;">Listen/Watch on Click...</p>
+  </div>
+
+  
+<br>
+
+<div class="container" style="background-color:#3E423E;border-radius:10px;">
+  <h3 style="color:#282E28;">Click to open in new tab.</h3>
+  <hr/>
+  <br/>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Aapki Aakhon Me Kuchh.mp4" target="_blank">1. Aapki Aakhon Me Kuchh</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Ambarsariya Fukrey Movie Full HD Video Song By VVAIDYA SAHAB.mp4" target="_blank">2. Ambarsariya Fukrey Full Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Anwar - Maula Mere Maula Aankhein Teri Kitni Haseen Ke Inka Aashique - RoopKumar Rathod.mp4" target="_blank">3. Anwar - Maula Mere Maula Aankhein Teri Kitni Haseen</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Carlos Vives, Shakira - La Bicicleta (Official Video).mp4" target="_blank">4. Carlos Vives, Shakira - La Bicicleta </a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Cham Cham Full Video - BAAGHI - Tiger Shroff, Shraddha Kapoor- Meet Bros, Monali Thakur- Sabbir Khan.mp4" target="_blank">5. Cham Cham Full Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Charlie Puth - Dangerously [Official Video].mp4" target="_blank">6. Charlie Puth - Dangerously Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Coldplay - Hymn For The Weekend (Official video).mp4" target="_blank">7. Coldplay - Hymn For The Weekend Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Coldplay - Paradise (Official Video).mp4" target="_blank">8. Coldplay - Paradise Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Deewani Mastani - Official Video Song - Bajirao Mastani - Deepika Padukone, Ranveer Singh, Priyanka.mp4" target="_blank">9. Deewani Mastani Video Song - Bajirao Mastani</a></b></pre> 
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Dua Lipa - New Love.mp4" target="_blank">10. Dua Lipa - New Love Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Galantis - No Money (Official Video).mp4" target="_blank">11. Galantis - No Money Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Icona Pop - Brightside [OFFICIAL VIDEO].mp4" target="_blank">12. Icona Pop - Brightside VIDEO Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Jee Le Full Song - Luck Ft. Imran Khan - Shruti Hassan.mp4" target="_blank">13. Jee Le Full Song - Luck Ft. Imran Khan - Shruti Hassan</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Jigar Da Tukda - Full Song - Ladies vs Ricky Bahl - Ranveer Singh - Parineeti Chopra.mp4" target="_blank">14. Jigar Da Tukda Video Song - Ladies vs Ricky Bahl</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Jo Bheji Thi Duaa Shanghai Full Song - Emraan hashmi, Abhay Deol, Kalki Koechlin.mp4" target="_blank">15. Jo Bheji Thi Duaa Shanghai Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Kahan Se Aye Badra - Yesudas & Haimanti Shukla - Chashm-e-Buddoor.mp4" target="_blank">16. Kahan Se Aye Badra - Yesudas & Haimanti Shukla</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Kun Faya Kun Full Video Song Rockstar - Ranbir kapoor.mp4" target="_blank">17. Kun Faya Kun Full Video Song Rockstar</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Kygo - Firestone ft. Conrad Sewell.mp4" target="_blank">18. Kygo - Firestone ft. Conrad Sewell</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Mast Qalandar by Mustafa khan.mp4" target="_blank">19. Mast Qalandar by Mustafa khan</a></b></pre> 
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Pardesi [Full Song] Dev D.mp4" target="_blank">20. Pardesi [Full Song] Dev D</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Rock On!!- Music Video- Pichhle Saat Dinon Mein.mp4" target="_blank">21. Rock On!!- Music Video- Pichhle Saat Dinon Mein</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Samjhawan Unplugged - Humpty Sharma Ki Dulhania - Singer- Alia Bhatt.mp4" target="_blank">22. Samjhawan Unplugged - Humpty Sharma Ki Dulhania - Singer- Alia Bhatt</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Sham - Aisha - Sonam Kapoor - Abhay Deol - Lisa Haydon.mp4" target="_blank">23. Sham - Aisha - Sonam Kapoor - Abhay Deol - Lisa Haydon</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Sia - Cheap Thrills (Performance Edit).mp4" target="_blank">24. Sia - Cheap Thrills (Performance Edit)</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Sia - The Greatest.mp4" target="_blank">25. Sia - The Greatest</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Sigala - Easy Love.mp4" target="_blank">26. Sigala - Easy Love</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/'Sunny Sunny Yaariyan' Full Video Song (Film Version) - Himansh Kohli, Rakul Preet.mp4" target="_blank">27. 'Sunny Sunny Yaariyan' Video Song</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Tera Chehra Full Video Song - Sanam Teri Kasam.mp4" target="_blank">28. Tera Chehra Full Video Song - Sanam Teri Kasam</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/The Breakup Song - Ae Dil Hai Mushkil - Ranbir - Anushka - Pritam - Arijit I Badshah - Jonita.mp4" target="_blank">29. The Breakup Song - Ae Dil Hai Mushkil</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/The Weeknd - Starboy (official) ft. Daft Punk.mp4" target="_blank">30. The Weeknd - Starboy (official) ft. Daft Punk</a></b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b><a href="Songs/Ye Dooriyan -  complete movie, in a single song..mp4" target="_blank">31. Ye Dooriyan - Video Song</a></b></pre>
+  
+  </div>
+<br/>
+
+<footer class="container-fluid text-center" style="background-color: #222222;color:white;bottom:0px;">
+  <p style="opacity:0.8;">Visit Archive</p>
+</footer>
+
+</body>
+</html>
