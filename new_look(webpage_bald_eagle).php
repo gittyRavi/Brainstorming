caa9d6ef6a5bb493d696ca3408d9a1b9981a515e
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:29:22 +0530

    Create new_look(webpage_bald_eagle).php

---
+++ new_look(webpage_bald_eagle).php
@@ -0,0 +1,208 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Animals</TITLE>
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Animals</z>
+</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+				
+			center>div>img
+				{
+					border-radius:10%;
+					height:90px;
+				}
+
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<body>
+<H1 class="class1">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:#B4A113;width:90%;padding:3%"><img src="http://www.atozkidsstuff.com/images/birdsofprey/eagles/bald_eagle_chicks.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://res.freestockphotos.biz/pictures/16/16555-a-banded-adult-bald-eagle-pv.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://s-media-cache-ak0.pinimg.com/564x/bc/62/ad/bc62adb1e5884d842fdd98070be499e9.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://alaskazoo.org/sites/alaskazoo.org/files/bald%20eagle2.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.kansas.com/news/local/d2hc30/picture5657085/ALTERNATES/FREE_640/010815eagles_th1.JPG" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1>
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class2">
+Bald Eagle
+</H1>
+</td>
+<p>
+<!--<pre>-->
+<div style="background-color:green;padding:5px;border-radius:5px;overflow:hidden"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%">This article is about the bird. For other uses, see Bald eagle (disambiguation).
+"American eagle" redirects here. For other uses, see American eagle (disambiguation).
+
+The bald eagle (Haliaeetus leucocephalus, from Greek hali "sea", aiētos "eagle", leuco "white", cephalos "head") is a bird of prey found in North America. A sea eagle, it has two known subspecies and forms a species pair with the white-tailed eagle (Haliaeetus albicilla). Its range includes most of Canada and Alaska, all of the contiguous United States, and northern Mexico. It is found near large bodies of open water with an abundant food supply and old-growth trees for nesting.
+
+The bald eagle is an opportunistic feeder which subsists mainly on fish, which it swoops down and snatches from the water with its talons. It builds the largest nest of any North American bird and the largest tree nests ever recorded for any animal species, up to 4 m (13 ft) deep, 2.5 m (8.2 ft) wide, and 1 metric ton (1.1 short tons) in weight.[2] Sexual maturity is attained at the age of four to five years.
+
+Bald eagles are not actually bald; the name derives from an older meaning of the word, "white headed". The adult is mainly brown with a white head and tail. The sexes are identical in plumage, but females are about 25 percent larger than males. The beak is large and hooked. The plumage of the immature is brown.
+
+The bald eagle is both the national bird and national animal of the United States of America. The bald eagle appears on its seal. In the late 20th century it was on the brink of extirpation in the contiguous United States. Populations have since recovered and the species was removed from the U.S. government's list of endangered species on July 12, 1995 and transferred to the list of threatened species. It was removed from the List of Endangered and Threatened Wildlife in the Lower 48 States on June 28, 2007.
+
+<<--------------------------------------Description---------------------------------------->>
+
+The plumage of an adult bald eagle is evenly dark brown with a white head and tail. The tail is moderately long and slightly wedge-shaped. Males and females are identical in plumage coloration, but sexual dimorphism is evident in the species, in that females are 25% larger than males.[2] The beak, feet and irises are bright yellow. The legs are feather-free, and the toes are short and powerful with large talons. The highly developed talon of the hind toe is used to pierce the vital areas of prey while it is held immobile by the front toes.[3] The beak is large and hooked, with a yellow cere.[4] The adult bald eagle is unmistakable in its native range. The closely related African fish eagle (H. vocifer) (from far outside the bald eagle's range) also has a brown body, white head and tail, but differs from the bald in having a white chest and black tip to the bill.[5]
+
+The plumage of the immature is a dark brown overlaid with messy white streaking until the fifth (rarely fourth, very rarely third) year, when it reaches sexual maturity.[2][3] Immature bald eagles are distinguishable from the golden eagle (Aquila chrysaetos), the only other very large, non-vulturine bird in North America, in that the former has a larger, more protruding head with a larger beak, straighter edged wings which are held flat (not slightly raised) and with a stiffer wing beat and feathers which do not completely cover the legs. When seen well, the golden eagle is distinctive in plumage with a more solid warm brown color than an immature bald eagle, with a reddish-golden patch to its nape and (in immature birds) a highly contrasting set of white squares on the wing.[6] Another distinguishing feature of the immature bald eagle over the mature bird is its black, yellow-tipped beak; the mature eagle has a fully yellow beak.
+
+The bald eagle has sometimes been considered the largest true raptor (accipitrid) in North America. The only larger species of raptor-like bird is the California condor (Gymnogyps californianus), a New World vulture which today is not generally considered a taxonomic ally of true accipitrids.[7] However, the golden eagle, averaging 4.18 kg (9.2 lb) and 63 cm (25 in) in wing chord length in its American race (A. c. canadensis), is merely 455 g (1.003 lb) lighter in mean body mass and exceeds the bald eagle in mean wing chord length by around 3 cm (1.2 in).[5][8] Additionally, the bald eagle's close cousins, the relatively longer-winged but shorter-tailed white-tailed eagle and the overall larger Steller's sea eagle (H. pelagicus), may, rarely, wander to coastal Alaska from Asia.[5]
+</textarea> <img src="http://www.gauntlet.info/maps/baldeagle_map.jpg" style="height:23%;width:relative"> <img src="https://www.fws.gov/Midwest/eagle/images/nesttree.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Bald_eagle">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</BODY>
+</HTML>
