594d9c587c771f16b328940869556c32b8de4bf4
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:51:46 +0530

    Create arctic_region.php

---
+++ arctic_region.php
@@ -0,0 +1,235 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("http://i.huffpost.com/gen/2710254/images/o-ARCTIC-facebook.jpg");
+						background-width:device-width;
+						background-position: center center;
+						background-size:100%;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class5" style="opacity:0.8;color:#1BA017;">Region-(Arctic Northern Hemisphere)-Alpine&Frost/Tundra/Taiga</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:#407B7C;width:90%;padding:3%;background: rgb(64, 123, 124);background: rgba(64, 123, 124, 0.7);"><img src="http://img.huffingtonpost.com/asset/scalefit_630_noupscale/5734395d22000042072565d9.jpeg?cache=y9qwkdbc6m" alt="Image" style="width:19.2%;height:relative;"><img src="http://ichef.bbci.co.uk/naturelibrary/images/ic/credit/640x395/a/ar/arctic/arctic_1.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.realclearworld.com/blog/arctcicchina.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://2.bp.blogspot.com/_AIkWSJ039r8/S-kEZ09rZCI/AAAAAAAAB5E/9pkT3cbI3PU/s1600/arc3.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.russkiymir.ru/upload/medialibrary/fcf/fcfb47e20c12f9c96a61fd1caef8444e.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class7" style="opacity:0.8;color:#10557C;">
+Arctic Ecosystem
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div style="background-color:grey;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:#BCF72E;background-color:#2FC8D3;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+Ten Facts about the Arctic!
+
+Wrap up warm, gang, because we're off an icy adventure. Check out these ten brrr-illiant facts about the Arctic!
+
+1. The Arctic is located at the northernmost part of our planet. Scientists usually define the Arctic as the area above the 'Arctic Circle' — an imaginary line that circles around the top of the globe. 
+
+2. The Arctic consists of the Arctic Ocean and parts of Canada, Russia, the USA, Greenland, Norway, Finland, Sweden and Iceland. 
+
+3. Because of the Earth’s tilt, for at least one day a year there’s an entire day of darkness in this freezing region — and also a full day of sunshine. Imagine that!
+
+4. Temperatures as low as -70°C have been recorded in northern Greenland. Brrrrr!
+
+5. Despite the freezing-cold temperatures, approximately four million people call this wintery wonderland home! Amongst these are the indigenous people of the Arctic, called the 'Inuits'. They’ve found ingenious ways to survive in one of the harshest environments on our planet. 
+
+6. In 1958, a submarine called the USS Nautilus sailed beneath the frozen ice of the Arctic Ocean. This was proof that the enormous ice sheet rests on water and not land.
+
+7. The Arctic is home to lots of wonderful wildlife, including polar bears, Arctic foxes, walruses, seals and whales!
+
+8. One fascinating species found only in the Arctic is the narwhal, often referred to as the ‘unicorn of the sea’. Why? Well, male narwhals have a straight tusk projecting from the front of their head that can grow to over 3m in length!
+
+9. The word 'Arctic' comes from the Greek word for bear, Arktos. But this isn’t because of the polar bears! It’s believed the name refers to two constellations that can be seen in the northern sky — ‘Ursa Minor’ (Little Bear) and ‘Ursa Major’ (Great Bear).
+
+10. The ice of the Arctic contains around ten percent of the world’s fresh water. This giant, white, frozen reservoir reflects sunlight, helping keep the region cool. It also plays a super-important role in keeping our global climate stable.  
+</textarea> <img src="https://www.expeditions.com/globalassets/destinations/itineraries/thumbnails/norway-and-scotland-fjordlands-to-the-inner-hebrides_shutterstock_216508345_590x482.jpg" style="height:23%;width:relative"> <img src="http://b1ca250e5ed661ccf2f1-da4c182123f5956a3d22aa43eb816232.r10.cf1.rackcdn.com/contentItem-799813-4872600-hm953zuvr11n7-or.jpg" style="height:23%;width:relative"> <img src="https://www.adventuresmithexplorations.com/stuff/contentmgr/files/23/5a62f5637d153a5fc2dbfc21495c2c53/image/_resized/80_745_420_istock_000016207566_1118x630.jpg" style="height:23%;width:relative"> <img src="http://www.telegraph.co.uk/content/dam/Travel/Destinations/arctic/arctic-fox-large.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Arctic">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
