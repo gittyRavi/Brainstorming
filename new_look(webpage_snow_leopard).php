d983cf3638cc18e807d5b3bf94344e21f5609cd5
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:40:36 +0530

    Create new_look(webpage_snow_leopard).php

---
+++ new_look(webpage_snow_leopard).php
@@ -0,0 +1,208 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Animals</TITLE>
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Animals</z>
+</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					height:90px;
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<body>
+<H1 class="class1">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:red;width:90%;padding:3%"><img src="http://assets.worldwildlife.org/photos/4304/images/story_full_width/WEB_257721.jpg?1370438053" alt="Image" style="width:19.2%;height:relative;"><img src="http://napifalat.hu/wp-content/uploads/2013/06/hoparduc-05-800x449.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7pS2VP2wnKyN8wxywmXtY0-FwsgxoQuX1aWbTzupFB85LZ0CYrbMIUZykoYtBmmSRAeM78eb/" alt="Image" style="width:19.2%;height:relative;"><img src="http://assets.worldwildlife.org/photos/2204/images/story_full_width/Photo_2.jpg?1345642949" alt="Image" style="width:19.2%;height:relative;"><img src="http://plaza.ufl.edu/lindz615/snowleopard/snow.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1>
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class6">
+Snow Leopard
+</H1>
+</td>
+<p>
+<!--<pre>-->
+<div style="background-color:magenta;padding:5px;border-radius:5px;overflow:hidden"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%">This article is about the cat. For other uses, see Snow Leopard (disambiguation).
+
+The snow leopard or ounce (Panthera uncia syn. Uncia uncia) is a large cat native to the mountain ranges of Central and South Asia. It is listed as Endangered on the IUCN Red List of Threatened Species because, as of 2003, the size of the global wild population was estimated at 4,080–6,590 adults. Fewer than 2,500 individuals may be reproducing in the wild.[1] As of 2016, estimates for the size of the global population vary from at least 4,080 to about 8,700 individuals.[3]
+Snow leopards inhabit alpine and subalpine zones at elevations from 3,000 to 4,500 m (9,800 to 14,800 ft). In the northern range countries, they also occur at lower elevations.[4]
+Taxonomically, the snow leopard has been classified as Uncia uncia since the early 1930s.[2] Based on genotyping studies, the cat has been considered a member of the genus Panthera since 2008.[1][5] Two subspecies have been attributed, but genetic differences between the two have not been settled.[1]
+
+The snow leopard is also known in its native lands as "wāwrīn pṛāng" (Pashto: واورين پړانګ‎), "shan" (Ladakhi), "zigsa" (Tibetan), "irves" (Mongolian: ирвэс), "bars" or "barys" (Kazakh: барыс [ˈbɑrəs]), "ilbirs" (Kyrgyz: Илбирс), "barfānī chītā" Urdu: برفانی چیتا) and "him tendua" (Sanskrit, Hindi: हिम तेन्दुआ).
+According to the American Heritage Dictionary, the origin of the word panthera is unknown. A folk etymology derives the word from the Greek πάν pan ("all") and thēr (beast of prey) because they can hunt and kill almost anything. It was proposed to have come ultimately into Greek from a Sanskrit word meaning "the yellowish animal" or "whitish-yellow". The Greek word πάνθηρ, pánthēr, referred to all spotted felines generically.
+
+The species was first described by the German naturalist Johann Christian Daniel von Schreber on the basis of an illustration in his 1777 publication Die Säugethiere in Abbildungen nach der Natur mit Beschreibungen. Schreber named the cat Felis uncia and gave its type locality as Barbary, Persia, East India, and China.[8] In 1854, the British zoologist John Edward Gray proposed the genus Uncia, to which he subordinated the snow leopard under the name Uncia irbis.[9] British zoologist Reginald Innes Pocock corroborated this classification, but attributed the scientific name Uncia uncia. He also described morphological differences between snow leopards and the then-accepted members of Panthera.[10]
+The snow leopard is part of the Panthera lineage, one of the eight lineages of Felidae.
+
+<<--------------------------------------Description---------------------------------------->>
+
+Snow leopards are slightly smaller than the other big cats but, like them, exhibit a range of sizes, generally weighing between 27 and 55 kg (60 and 121 lb), with an occasional large male reaching 75 kg (165 lb) and small female of under 25 kg (55 lb).[19][20] They have a relatively short body, measuring in length from the head to the base of the tail 75 to 150 cm (30 to 60 in).[21] However, the tail is quite long, at 80 to 100 cm (31 to 39 in), with only the domestic-cat-sized marbled cat being relatively longer-tailed.[22][23] They are stocky and short-legged big cats, standing about 60 cm (24 in) at the shoulder.[24]
+Snow leopards have long, thick fur, and their base color varies from smoky gray to yellowish tan, with whitish underparts. They have dark grey to black open rosettes on their bodies, with small spots of the same color on their heads and larger spots on their legs and tails. Unusually among cats, their eyes are pale green or grey in color.[22][23]
+Snow leopards show several adaptations for living in a cold, mountainous environment. Their bodies are stocky, their fur is thick, and their ears are small and rounded, all of which help to minimize heat loss. Their paws are wide, which distributes their weight better for walking on snow, and have fur on their undersides to increase their grip on steep and unstable surfaces; it also helps to minimize heat loss. Snow leopards' tails are long and flexible, helping them to maintain their balance, which is very important in the rocky terrain they inhabit. Their tails are also very thick due to fat storage and are very thickly covered with fur, which allows them to be used like a blanket to protect their faces when asleep.[23][25]
+The snow leopard has a short muzzle and domed forehead, containing unusually large nasal cavities that help the animal breathe the thin, cold air of their mountainous environment.[22]
+</textarea> <img src="http://www.nationalgeographic.fr/wp-content/uploads/2014/01/MAP_LOC_Leo.jpg" style="height:23%;width:relative"> <img src="http://slavyanskaya-kultura.ru/upload/comments/9d5b2b20a97c90104f0d36cd4cbd275a.jpg.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Snow_leopard">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</BODY>
+</HTML>
