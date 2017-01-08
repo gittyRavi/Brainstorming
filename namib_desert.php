8276f97e2c30a38d151b2f0f87e7f16ec19b5d8b
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:45:54 +0530

    Create namib_desert.php

---
+++ namib_desert.php
@@ -0,0 +1,233 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("https://s-media-cache-ak0.pinimg.com/736x/5d/aa/bd/5daabdccbf33d0a06e5dacd978bf23d8.jpg");
+						background-width:device-width;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class3" style="opacity:0.8;">Region-(Southern Africa)-Desert</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:red;width:90%;padding:3%;background: rgb(255, 100, 0);background: rgba(255, 100, 0, 0.5);"><img src="http://backwallpapers.com/wp-content/uploads/2016/08/Namib-Desert-HQ-Image-New-1224.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://s-media-cache-ak0.pinimg.com/564x/a6/21/fa/a621fa76e9001b97f1bd8760aecafbfe.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://infoshutter.com/wp-content/uploads/2015/06/Desert-and-Ocean-10.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.redfernnaturalhistory.com/wp-content/uploads/2016/09/Namibia-Trip-1.png" alt="Image" style="width:19.2%;height:relative;"><img src="https://s-media-cache-ak0.pinimg.com/564x/1a/dc/69/1adc692badf6869a4abee8c5740c8a7f.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class6" style="opacity:0.8;color:orange;">
+Namib Desert
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div style="background-color:purple;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:white;background-color:orange;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+The Namib Desert is a coastal desert in Namibia and southwestern Angola that encompasses the world's highest sand dunes, gravel plains, and rugged mountains. It stretches some 1,200 miles (1,900 km) along the Atlantic coast of Namibia, which is named after this desert. The name "Namib" is from a word in the Nama language that means "immense." The desert occupies an area of around 31,200 m² (80,000 km²), though its east–west width varies from 30 to 100 miles (50–160 km). The area is considered to be the oldest desert in the world, having endured arid or semi-arid conditions for an estimated 55 to 80 million years. The Namib is an important location for the mining of diamonds, tungsten, and salt. The diamonds, which are alluvial, are found in beds of gravel.
+
+The desert's aridity is caused by the descent of dry, warm air from the east that is cooled by the cold Benguela current along the coast. The upper layer of warm, dry air prevents the cool, humid air from rising and forming clouds. Some areas have less than half an inch (10 mm) of rain annually and are almost completely barren.
+
+History
+The Namib Desert is believed to be the world’s oldest desert and it has been arid for at least 55 million years. The convergence of the Benguela upwelling and the hot interior have maintained, and perhaps increased, this aridity in recent times, but they did not generate the aridity. The region, isolated between the ocean and the escarpment, is considered to be a constant island of aridity surrounded by a sea of climatic change. The arid conditions probably started with the continental split of West Gondwana 130 million to 145 million years ago when this area shifted to its present position along the Tropic of Capricorn. This lengthy dry period has had a profound influence on the region’s biodiversity. The region has remained a relatively stable center for the evolution of desert species. This has resulted in a unique array of biodiversity with high levels of endemism and numerous advanced adaptations to arid conditions.[1]
+
+Flora and fauna
+The Namib Desert is home to a number of unusual species of plants and animals that are found nowhere else in the world. One of these is Welwitschia mirabilis, a shrub-like plant that grows just two long leaves continuously throughout its lifetime. These leaves may grow to be several meters long and over time become gnarled and twisted from the desert winds. They are the longest-lived leaves of any member of the plant kingdom. It is estimated that the largest of these plants are about 2,500 years old.
+
+Dense fogs along the coast and the freshwater they contain are the life essence of the Namib. The dew that condenses on desert grasses and on the bodies of smaller creatures is what sustains many of these animals. Others take advantage of the few muddy water holes, which may draw gemsbok, ostriches, sand grouse, vultures, and warthogs.
+
+Most of the animals and plants that survive in the Namib do so by unique adaptations. The gemsbok, for example, also known as the oryx, has a lighter coat than any long-horned antelope subspecies and an intricate system of blood vessels and nasal passages that cool its blood before it reaches the brain. They dig for underground water, drink from watering holes, and feed in the early morning on plants with high moisture content. Its numerous adaptations allow it to tolerate high temperatures and go days without water.
+
+There are almost 70 reptile species in the ecoregion, of which five are found only in the Namib Desert. The desert is also home to a large number of small rodent species that occur among the rocky habitats in the western deserts, in the sand dunes, and in the vegetation of the gravel plains.
+
+Namib-Naukluft National Park, one of Africa's largest, contains a group of huge sand dunes, which at nearly a thousand feet (300 meters) high are the tallest sand dunes in the world. During the rainy season in some years, the waters of the Tsauchab River form pools at the base of the dunes. Sossusvlei is a great dry clay marsh (“vlei”). “Sossus” means “blind river” in the Nama language, so the literal translation of the word Sossusvlei is “marsh of the blind river,” that is, “marsh of the river that does not have a mouth toward the sea.”
+
+The interaction between the water-laden air coming from the sea via southerly winds, some of the strongest of any coastal desert, and the dry air of the desert causes immense fogs and strong currents in which sailors easily lose their way. Along with the Skeleton Coast farther north, the area is notorious as the site of many shipwrecks. Some of these wrecked vessels can be found as much as 50 meters inland, as the desert is slowly creeping westward into the sea, reclaiming land over a period of many years.
+
+The border of the desert to the north and east is the foot of the Great Western Escarpment of Namibia, atop which more humid lands prevail. But in the south, a gradual transition merges the Namib with the Karoo and Kalahari deserts of the interior.
+
+A curious feature of the Namib Desert is circles on the ground that are accentuated by abrupt changes in the vegetation pattern. These circles, known as "fairy rings," are most noticeable from the air. There has been interest and speculation as to their origin for centuries. It is now thought that they are created by a termite.
+</textarea> <img src="http://2.bp.blogspot.com/-3dZIL61h2kE/VfYJ3DYpbVI/AAAAAAAABFE/kEkwwKxLoN8/s1600/Desert%2B%252810%2529.jpg" style="height:23%;width:relative"> <img src="http://originaltravel.assets.d3r.com/images/gallery/257665-skeleton-coast-2.jpg" style="height:23%;width:relative"> <img src="http://afrika-raj.ru/images/zhizn-v-pustine-namib1.jpg" style="height:23%;width:relative"> <img src="http://images.meltour.com/image/default//aventure-caprivi-zambeze-namibie-sossusvlei.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Namib_Desert">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
