3ae9cf23e5b8339a3bce6c42d5bd5b55e759c256
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:45:07 +0530

    Create sunderbans.php

---
+++ sunderbans.php
@@ -0,0 +1,228 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("https://online.science.psu.edu/sites/default/files/biol011/Fig-15-Earth-Biomes.jpg");
+						background-width:device-width;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class1" style="opacity:0.8;">Region-(South Asia)-Delta</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;width:90%;padding:3%;position:relative;background: rgba(153, 0, 153);background: rgba(153, 0, 153, 0.6);"><img src="https://s-media-cache-ak0.pinimg.com/564x/ef/94/5d/ef945d6a0b0930d93d1663a283bc362c.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://s-media-cache-ak0.pinimg.com/564x/c5/f5/3a/c5f53aca8cbb9207795ed817124927e7.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://blog.tour.com.bd/wp-content/uploads/2015/03/Sunderban1.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://s-media-cache-ak0.pinimg.com/564x/35/a8/d9/35a8d9491d538e6aeb4df6fa026ba7c6.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://s-media-cache-ak0.pinimg.com/564x/34/3a/cf/343acf6a7e43d1693ab3636f261b01ea.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class4" style="opacity:0.8;">
+Sunderbans
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div  style="background-color:brown;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:blue;background-color:yellow;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+Sundarban, the largest delta in the world, consists of 10,200 sq km of Mangrove Forest , spread over India (4200 sq km of Reserved Forest ) and Bangladesh (6000 sq km approx of Reserved Forest ) and is also the largest Mangrove Forest in the world. Another 5400 sq km of non-forest, inhabited region in India , along the north and north-western fringe of mangrove forest, is also known as Sundarban region in India . Hence, the total area of Sundarban region in India is 9600 sq km which constitutes the Sundarban Biosphere Reserve. Indian Sundarban is bound on the west by river Muriganga and on the east by rivers Harinbhahga and Raimangal. Other major rivers flowing through this eco-system are Saptamukhi, Thakuran, Matla and Goasaba.
+
+	
+Uniqueness of Sundarban Forest
+Sundarban Biosphere Reserve was constituted by Government of India (GOI)in 1989 and it received the recognition of UNESCO under its Man and Biosphere (MAB) Programme in November, 2001. Sundarban National Park , forming the core area of Sundarban Tiger Reserve, received recognition as World Heritage Site by UNESCO in 1987. It has been nominated by GOI for recognition as Ramsar Site (a wetland of international importance). Sundarban Tiger Reserve was constituted by GOI under Project Tiger scheme, in 1973. Sundarban is the only mangrove forest in the world which is the home of Tiger. Sundarban Tiger Reserve has the highest tiger population in the world.
+
+	
+Importance of Sundarban Ecosystem
+Sundarban has extremely rich diversity of aquatic and terrestrial flora and fauna. Sundarban's highly productive ecosystem acts as a natural fish nursery. Sundarban Mangrove reduces the fury of cyclonic storm and prevents erosion due to tidal action. Finally, millions of people depend on Sundarban Ecosystem for their livelihood and sustenance through fishing, collection of honey and fuelwood/timber.
+
+	
+Topography and Soil
+Geologically, the Sundarban delta is the largest prograding delta on the globe. The region is covered solely by quaternary sediments carried and deposited by the rivers Ganges , Matla & Bidyadhari.  
+
+ 
+Climate and Rainfall
+Although the region is situated south of the Tropic of Cancer, the temperature is equable due to its proximity to the sea. Average annual maximum temperature is around 35 C . Average annual rainfall is 1920 mm. Average humidity is about 82% which is more or less uniform throughout the year.
+
+</textarea> <img src="http://sundarbanchalo.com/wp-content/uploads/freshizer/c525510e3768c9790e3cd30fa2a10059_Sundarbans-1500-600-c.jpg" style="height:23%;width:relative"> <img src="https://s-media-cache-ak0.pinimg.com/564x/ab/3d/37/ab3d378ff8737d1b146ba24b4c759729.jpg" style="height:23%;width:relative"> <img src="https://s-media-cache-ak0.pinimg.com/564x/f9/cc/b9/f9ccb97147533ea54aab275b46db6964.jpg" style="height:23%;width:relative"> <img src="http://www.neoplanete.fr/wp-content/uploads/2012/06/Xavier-Desmier-5-1024x681.jpg" style="height:23%;width:relative"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Sundarbans">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
