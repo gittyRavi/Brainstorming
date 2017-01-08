d464b4c74d0ba4024982bc903ac04da46e34005c
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:38:22 +0530

    Create new_look(webpage_bengal_tiger).php

---
+++ new_look(webpage_bengal_tiger).php
@@ -0,0 +1,217 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Animals</TITLE>
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Animals</z>
+</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					height:90px;
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<body>
+<H1 class="class1">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:darkorange;width:90%;padding:3%"><img src="http://dinoanimals.com/wp-content/uploads/2014/01/Bengal-tiger-12.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://cache2.allpostersimages.com/p/LRG/27/2756/1T9TD00Z/plakater/jones-adam-white-phase-of-the-bengal-tiger.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://animalwall.xyz/wp-content/uploads/2016/03/cats-bengal-tiger-predator-wildlife-resting-cat-images-736x460.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://1.bp.blogspot.com/-bqrhaSa0zbs/UGlK80plwuI/AAAAAAAAAXw/9cMuyQDfb78/s1600/sunderbans_tiger.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://assets.worldwildlife.org/photos/1601/images/hero_small/Bengal_Tiger_8.9.2012_Hero_and_Circle_XL_257678.jpg?1345598774" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1>
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class4">
+Bengal Tiger
+</H1>
+</td>
+<p>
+<!--<pre>-->
+<div style="background-color:grey;padding:5px;border-radius:5px;overflow:hidden"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%">For other uses of 'Bengal tiger' and related terms, see Bengal tiger (disambiguation). For other uses of 'Royal Bengal tiger' and related terms, see Royal Bengal tiger (disambiguation).
+
+The Bengal tiger (Panthera tigris tigris) is the most numerous tiger subspecies. By 2011, the total population was estimated at fewer than 2,500 individuals with a decreasing trend. None of the 'Tiger Conservation Landscapes' within the Bengal tiger's range is considered large enough to support an effective population size of 250 adult individuals. Since 2010, it is listed as Endangered on the IUCN Red List.[1]
+
+As of 2010, Bengal tiger populations in India have been estimated at 1,706–1,909.[2] As of 2014, they had reputedly increased to an estimated 2,226 individuals,[3] but the method used in the census may not be accurate.[4][5]
+Bengal tigers number around 440 in Bangladesh and 163–253 in Nepal.[6][7][8] Prior censuses placed the population of tigers in Bhutan at around 65-75 individuals, however, the latest census estimated that 103 wild Bengal tigers are living in the country.[9]
+Bengal is traditionally fixed as the typical locality for the binomen Panthera tigris, to which the British taxonomist Reginald Innes Pocock subordinated the Bengal tiger in 1929 under the trinomen Panthera tigris tigris.[10] The Bengal, Caspian and Siberian tigers, and lion rank among the biggest cats.[11][12][13]
+It is the national animal of both India and Bangladesh.[14]
+
+Characteristics
+
+The Bengal tiger's coat is yellow to light orange, with stripes ranging from dark brown to black; the belly and the interior parts of the limbs are white, and the tail is orange with black rings. The white tiger is a recessive mutant of the Bengal tiger, which is reported in the wild from time to time in Assam, Bengal, Bihar and especially from the former State of Rewa. However, it is not to be mistaken as an occurrence of albinism. In fact, there is only one fully authenticated case of a true albino tiger, and none of black tigers, with the possible exception of one dead specimen examined in Chittagong in 1846.[15]
+Male Bengal tigers have an average total length of 270 to 310 cm (110 to 120 in) including the tail, while females measure 240 to 265 cm (94 to 104 in) on average.[12] The tail is typically 85 to 110 cm (33 to 43 in) long, and on average, tigers are 90 to 110 cm (35 to 43 in) in height at the shoulders.[16] The weight of males ranges from 180 to 258 kg (397 to 569 lb), while that of the females ranges from 100 to 160 kg (220 to 350 lb).[12] The smallest recorded weights for Bengal tigers are from the Bangladesh Sundarbans, where adult females are 75 to 80 kg (165 to 176 lb).[17] Bengal tigers have exceptionally stout teeth, and the canines are the longest among all living felids; measuring from 7.5 to 10 cm (3.0 to 3.9 in) in length.[18]
+Genetic ancestry[edit]
+Bengal tigers are defined by three distinct mitochondrial nucleotide sites and 12 unique microsatellite alleles. The pattern of genetic variation in the Bengal tiger corresponds to the premise that they arrived in India approximately 12,000 years ago.[19] This is consistent with the lack of tiger fossils from the Indian subcontinent prior to the late Pleistocene and the absence of tigers from Sri Lanka, which was separated from the subcontinent by rising sea levels in the early Holocene.[20]
+Body weight[edit]
+
+Female Bengal tiger in the Ranthambhore Tiger Reserve
+Bengal tigers may weigh up to 325 kg (717 lb) and reach a head and body length of 320 cm (130 in).[18] Several scientists indicated that adult male Bengal tigers from Nepal, Bhutan, and Assam, Uttarakhand and West Bengal in northern India (collectively, the tigers of the Terai) consistently attain more than 227 kg (500 lb) of body weight. Seven adult males captured in Chitwan National Park in the early 1970s had an average weight of 235 kg (518 lb) ranging from 200 to 261 kg (441 to 575 lb), and that of the females was 140 kg (310 lb) ranging from 116 to 164 kg (256 to 362 lb).[21] Males from northern India are nearly as large as Siberian tigers with a greatest length of skull of 332 to 376 mm (13.1 to 14.8 in).[22]
+Verifiable Sundarbans tiger weights are not found in any scientific literature. Forest Department records list weight measurements for these tigers, but none are verifiable and all are guesstimates. There are also reports of head and body lengths, some of which are listed as over 365.7 cm (144.0 in). More recently, researchers from the University of Minnesota and the Bangladesh Forest Department carried out a study for the US Fish and Wildlife Service and weighed three Sundarbans tigresses from Bangladesh. All three tigers were female, two of which were collared, captured and sedated, but the other one had been killed by local villagers. The two collared tigresses were weighed using 150 kg (330 lb) scales, and the tigress killed by villagers was weighed using a balance scale and weights. The two collared females both showed signs of teeth wear and both were between 12 and 14 years old. The tigress killed by the villagers was a young adult, probably between 3 and 4 years old, and she was likely a pre-territorial transient. The three tigresses had a mean weight of 76.7 kg (169 lb). One of the two older female's weight 75 kg (165 lb) weighed slightly less than the mean because of her old age and relatively poor condition at the time of capture. Skulls and body weights of Sundarbans tigers were found to be distinct from other subspecies, indicating that they may have adapted to the unique conditions of the mangrove habitat. Their small sizes are probably due to a combination of intense intraspecific competition and small size of prey available to tigers in the Sundarbans, compared to the larger deer and other prey available to tigers in other parts.[23]
+
+<<--------------------------------------Description---------------------------------------->>
+
+In 1982, a sub-fossil right middle phalanx was found in a prehistoric midden near Kuruwita in Sri Lanka, which is dated to about 16,500 ybp and tentatively considered to be of a tiger. Tigers appear to have arrived in Sri Lanka during a pluvial period during which sea levels were depressed, evidently prior to the last glacial maximum about 20,000 years ago.[29] In 1929, the British taxonomist Pocock assumed that tigers arrived in southern India too late to colonize Sri Lanka, which earlier had been connected to India by a land bridge.[10]
+In the Indian subcontinent, tigers inhabit tropical moist evergreen forests, tropical dry forests, tropical and subtropical moist deciduous forests, mangroves, subtropical and temperate upland forests, and alluvial grasslands. Latter tiger habitat once covered a huge swath of grassland and riverine and moist semi-deciduous forests along the major river system of the Gangetic and Brahmaputra plains, but has now been largely converted to agricultural land or severely degraded. Today, the best examples of this habitat type are limited to a few blocks at the base of the outer foothills of the Himalayas including the Tiger Conservation Units (TCUs) Rajaji-Corbett, Bardia-Banke, and the transboundary TCUs Chitwan-Parsa-Valmiki, Dudhwa-Kailali and Sukla Phanta-Kishanpur. Tiger densities in these TCUs are high, in part because of the extraordinary biomass of ungulate prey.[30]
+The Bengal tigers in the Sundarbans in India and Bangladesh are the only tigers in the world inhabiting mangrove forests. The population in the Indian Sundarbans is estimated as 70 tigers in total.[2]
+
+</textarea> <img src="https://sites.google.com/site/tdennis14/BengalHAbitat.jpg" style="height:23%;width:relative"> <img src="http://animal.memozee.com/ArchOLD-2/1109349759.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Bengal_tiger">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</BODY>
+</HTML>
