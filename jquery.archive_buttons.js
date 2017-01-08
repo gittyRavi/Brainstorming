22fa5ef75a3c4d63aa00d7dc35243f373ff06fa7
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:58:18 +0530

    Create jquery.archive_buttons.js

---
+++ jquery.archive_buttons.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+ 	$("#id5").click(function(){
+ 	 $("#id5").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+		setTimeout(function a(){window.location="learncplusplus.php";},2000);
+
+  });
+
+ 	$("#id6").click(function(){
+ 	 $("#id6").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+		setTimeout(function b(){window.location="learnjava.php";},2000);
+
+  });
+
+ 	$("#id7").click(function(){
+ 	 $("#id7").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+		setTimeout(function c(){window.location="ecomenu.php";},2000);
+
+  });
+
+ 	$("#id8").click(function(){
+ 	 $("#id8").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+		setTimeout(function d(){window.location="search_video.html";},2000);
+
+  });
+  
+	$("#yt-js-player").click(function(){
+		window.location="yt-js-player.html";
+  });
+ });
