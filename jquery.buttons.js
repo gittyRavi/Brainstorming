ac7313eea05387d6b82043fbcfe693e5a20a17ce
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 06:02:13 +0530

    Create jquery.buttons.js

---
+++ jquery.buttons.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("#home").click(function(){
+		window.location = "index.html";
+		return false;
+	});
+
+	$("#id1").click(function(){
+		window.location = "menu.php";
+		return false;
+	});
+	
+	$("#id2").click(function(){
+		window.location = "archive.php";
+		return false;
+	});
+	
+	$("#id3").click(function(){
+		window.location = "main_info.php";
+		return false;
+	});
+	
+	$("#id4").click(function(){
+		window.location = "contacts.php";
+		return false;
+	});
+});
