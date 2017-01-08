11d1009d4ad18ba901db5813ab357ad642f3ed23
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:50:57 +0530

    Create sahara_desert.php

---
+++ sahara_desert.php
@@ -0,0 +1,252 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("http://atalantatravel.ru/images/photoes/tun5.jpg");
+						background-width:device-width;
+						background-position: center center;
+						background-size:100%;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class5" style="opacity:0.8;color:red;">Region-(Northern Africa)-Desert</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:purple;width:90%;padding:3%;background: rgb(153, 0, 153);background: rgba(153, 0, 153, 0.6);"><img src="http://travelercorner.com/wp-content/uploads/2016/03/sahara-desert-algeria-600x450.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.place-des-vacances.com/assets/images/dossier/59-1455620626.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/14/pr/p014prww.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://evodevi.bursabi.com/wp-content/uploads/2015/08/sahra-%C3%A7%C3%B6l%C3%BC-iklim-%C3%B6zellikleri.jpg" alt="Green" alt="Image" style="width:19.2%;height:relative;"><img src="http://travelercorner.com/wp-content/uploads/2016/03/Camel-Riding-at-sahara-desert-africa-600x450.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class6" style="opacity:0.8;color:orange;">
+Sahara Desert
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div style="background-color:#FDDD54;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:#60E63D;background-color:#DB614D;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+The Sahara Desert
+
+Landscape, Water and Climate Information
+
+One of the most storied and unforgiving lands in the world, the Sahara -- the Arabic word for "desert" -- evokes a poignant sense of time and nature's power, of antiquity and legend, of wonder and mystery. It has been the setting for some of the most pivotal chapters in Western history.
+
+Location
+Blanketing much of the northern third of the African Continent, or some 3.5 million square miles, the Sahara Desert extends eastward from the Atlantic Ocean some 3,000 miles to the Nile River and the Red Sea, and southward from the Atlas Mountains of Morocco and the Mediterranean shores more than 1,000 miles to the savannah called the Sahel. More than 16 times the size of France, the Sahara Desert blankets nearly all of Mauritania, Western Sahara, Algeria, Libya, Egypt and Niger; the southern half of Tunisia; and the northern parts of Mali, Chad and Sudan.
+
+Landscape
+The Sahara's topographical features, said "The Living Africa," include not only the iconic sand dune fields, but also arid mountains, plateaus, sand- and gravel-covered plains, shallow basins and large oasis depressions. Its highest point is Chad's Mount Koussi (an extinct volcanic crater that rises 11,204 feet above sea level at the peak), and its lowest, Egypt's Qattera Depression (an oasis depression that lies 436 feet below sea level at the deepest point).
+
+The Sahara's fabled dune fields, which cover only about 15 percent of the entire desert's surface, lie primarily in the north central region, in the countries of Algeria and Libya. Most sand dunes -- mounds and ridges of sand sculpted by wind and gravity -- have a windward or "backslope" surface and a leeward or "slipface" surface. Prevailing winds drive sand up the backslope until it reaches a crest and then collapses under the pull of gravity, cascading down the slipface. The wind may leave rippled surfaces in its wake.
+
+The dunes take on varied and complex forms. A few examples include:
+
+Crescent dunes, which have arms or "horns" that point downwind and embrace the slip face. These dunes form under winds that have blown from the same direction for an extended period.
+Linear dunes, which have long, straight or slightly sinuous forms, that may extend for miles. They develop under winds that blow from either of two directions.
+Transverse dunes, which have sharply crested, roughly saber-shaped forms that lie parallel and may reach to nearly a thousand feet in height and extend for over a hundred miles in length. They form in alignment with a prevailing wind that has changed direction for a prolonged period and redistributed the sands of earlier dunes.
+Star dunes, which have central pyramidal mounds with three or more radiating arms, each with a backslope and a slipface. The star dunes form under winds that blow from several directions.
+Dome dunes, which have circular- or oval-shaped mounds, which, surprisingly, have no backslope or slipface. Comparatively rare, they tend to form at the upwind edges of dune fields.
+
+Water
+The Sahara has only two permanent rivers and a handful of lakes, but it has substantial underground reservoirs, or aquifers.
+
+Its permanent rivers are the Nile and the Niger. The Nile rises in central Africa, south of the Sahara, and flows northward through Sudan and Egypt and empties into the Mediterranean. The Niger rises in western Africa, southwest of the Sahara, and flows northeastward into Mali and the desert then turns southeastward, through Nigeria, and empties into the Gulf of Guinea.
+
+The Sahara has some 20 or more lakes, but only one with potable water--the expansive but shallow Lake Chad, a continually expanding and shrinking body of water that lies in the country of Chad, at the southernmost edge of the Sahara. Other lakes hold a briny stew of undrinkable water.
+
+The Sahara's aquifers often lie just below the surface of intermittent drainages, called "wadis," which rise in mountain ranges and empty onto the desert floor. The aquifers sometimes discharge some of their waters to the surface at locations called "oases," which are normally found in the lower points of surface depressions.
+
+Climate
+The Sahara has one of the world's most severe climates. Typically, the Sahara landscape experiences extremely limited to virtually no rainfall, powerful and capricious winds and wide temperature ranges.
+
+Across the desert, the annual average rainfall equals no more than a few inches or less, much less in many locations. In some areas, no rain at all may fall over several years. Then, several inches may fall in a torrential downpour. Then, no rain at all may fall for several more years.
+
+The prevailing wind, which blows from the northeast toward the equator throughout the year, accounts for the desert's aridness. As the wind moves southwestward, the air warms, dissipating moisture that might otherwise be released as rainfall. Locally, hot winds often lift sand and dust particles from the desert floor, spinning them upward through cooler air as dust devils or propelling them southwestward as fierce and blinding dust storms.
+
+In the summer, daytime air temperatures across the Sahara often soar to well over 100 degrees Fahrenheit, with the hottest air temperature meteorologists have ever recorded -- 136 degrees -- occurring at Azizia, Libya, on September 13, 1922. Under the clear skies, the temperature may fall 40 degrees or more during an evening. In the winter, freezing temperatures may occur in the northern Sahara, and milder temperatures, across the southern Sahara. Snow may fall occasionally in some of the higher mountain ranges and rarely, on the desert floor.
+
+</textarea> <img src="http://justfunfacts.com/wp-content/uploads/2016/03/sahara-desert-oasis.jpg" style="height:23%;width:relative"> <img src="http://travelercorner.com/wp-content/uploads/2016/03/Sahara-Desert-trees-600x338.jpg" style="height:23%;width:relative"> <img src="http://2.bp.blogspot.com/-6fNyMgaC_co/UQa5o9Dx8sI/AAAAAAAAMJg/CgRtk4iQiqw/s1600/Desert+Animals+(2).jpg" style="height:23%;width:relative"> <img src="http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/fennec-fox-hole.jpg.adapt.945.1.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Sahara">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
