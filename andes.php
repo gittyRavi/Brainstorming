5357f44824039f6af7cd2c00d923b16b8c1ffdaa
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:48:49 +0530

    Create andes.php

---
+++ andes.php
@@ -0,0 +1,275 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("http://www.artsfon.com/large/201511/77049.jpg");
+						background-width:device-width;
+						background-position: center center;
+						background-size:100%;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class3" style="opacity:0.8; color:yellow;">Region-(Americas)-Montane</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:#59858B;width:90%;padding:3%;background: rgb(89, 133, 139);background: rgba(89, 133, 139, 0.7);"><img src="http://t.wallpaperweb.org/wallpaper/nature/1600x1200/AndesMountainsPatagoniaArgentina.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.wallpapersxl.com/wallpapers/1600x1200/andes/312555/andes-chile-papel-de-parede-312555.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://s-media-cache-ak0.pinimg.com/736x/40/44/6c/40446ccb9bcf3d9c6bb1f488cc3cd89a.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://lh6.ggpht.com/_XtyyHTS1bNk/SViaa05y_mI/AAAAAAAAE_A/iqCiN49qyNs/s800/.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://greenywallpapers.com/resizes/6/21546-andes-mountains-mendoza-wallpapers-1600x1200.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class4" style="opacity:0.8;color:#20BB11;">
+Andes Mountains
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div style="background-color:#95FD56;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:#A7510D;background-color:#9DFBF8;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+The Andes is the longest mountain range in the world and boasts some of the highest peaks. The range is also known for its volcanoes, ruins of long-ago civilizations and the source of a malaria treatment.
+
+The origin of the name Andes has not been definitively determined; most agree that it comes from the Quechua word <em>anti</em>, which translates to &quot;high crest.&quot; A minority argue that it comes from Anti Suyu, which is one of the four regions of the Incas who once inhabited the range.
+
+The Andes Mountains stretch about 4,500 miles along the western coast of South America.
+
+Located along the entire western coast of South America, the Andes mountain range is about 4,500 miles (7,242 kilometers) long. The mountain range spans seven countries &mdash; Venezuela, Colombia, Ecuador, Peru, Bolivia, Chile and Argentina &mdash; and can be found between 10 degrees north and 57 degrees south latitudes and 70 degrees to 80 degrees west longitude. It is up to 500 miles (804 km) wide.
+
+The only mountains that are taller than the Andes are the <a href="http://www.livescience.com/23359-mount-everest.html">Himalaya Mountains</a> and their adjacent ranges, including the Hindu Kush. The average elevation in the Andes is about 13,000 feet (3,962 meters). The highest elevation in the Andes is Mount Aconcagua in Argentina, which is 22,841 feet (6,962 m) above sea level.
+
+The Andes are dotted with a number of depressions that separate the length of the range into several segments.
+
+Climate of the Andes
+
+Because the Andes act as a large wall between the Pacific Ocean and the continent, they have a tremendous impact on climate in the region. The northern part of the Andes is typically rainy and warm, and the weather is also wet in the eastern part of central Andes, and the area to the southwest. To the west, the dry climate is dominated by the Atacama Desert in northern Chile. The mountains form a rain cover over the eastern plains of Argentina, which have extremely dry weather.
+
+The Andes Mountains of South America are home to 99 percent of tropical glaciers ­— permanent rivers of ice at high enough elevations not to be affected by the types of balmy temperatures usually associated with the tropics.
+
+The Andes are also home to the world’s highest volcano when measured above sea level, Ojos del Salado on the Chile-Argentina border, which reaches 22,615 feet (6,893 m), and more than 50 volcanoes that reach 19,685 feet (6,000 m).
+
+ 54  120  121 MORE
+The south face of Aconcagua, the highest peak in South America, at 22,841 feet (6,962 meters) above sea level.
+The south face of Aconcagua, the highest peak in South America, at 22,841 feet (6,962 meters) above sea level.
+Credit: Johnathan Esper Shutterstock
+The Andes is the longest mountain range in the world and boasts some of the highest peaks. The range is also known for its volcanoes, ruins of long-ago civilizations and the source of a malaria treatment.
+
+The origin of the name Andes has not been definitively determined; most agree that it comes from the Quechua word anti, which translates to "high crest." A minority argue that it comes from Anti Suyu, which is one of the four regions of the Incas who once inhabited the range.
+
+The Andes Mountains stretch about 4,500 miles along the western coast of South America.
+The Andes Mountains stretch about 4,500 miles along the western coast of South America.
+Credit: CIA World Factbook
+Located along the entire western coast of South America, the Andes mountain range is about 4,500 miles (7,242 kilometers) long. The mountain range spans seven countries — Venezuela, Colombia, Ecuador, Peru, Bolivia, Chile and Argentina — and can be found between 10 degrees north and 57 degrees south latitudes and 70 degrees to 80 degrees west longitude. It is up to 500 miles (804 km) wide.
+
+The only mountains that are taller than the Andes are the Himalaya Mountains and their adjacent ranges, including the Hindu Kush. The average elevation in the Andes is about 13,000 feet (3,962 meters). The highest elevation in the Andes is Mount Aconcagua in Argentina, which is 22,841 feet (6,962 m) above sea level.
+
+The Andes are dotted with a number of depressions that separate the length of the range into several segments. [Image Gallery: Journey into the Tropical Andes]
+
+Climate of the Andes
+
+Because the Andes act as a large wall between the Pacific Ocean and the continent, they have a tremendous impact on climate in the region. The northern part of the Andes is typically rainy and warm, and the weather is also wet in the eastern part of central Andes, and the area to the southwest. To the west, the dry climate is dominated by the Atacama Desert in northern Chile. The mountains form a rain cover over the eastern plains of Argentina, which have extremely dry weather.
+
+The Andes Mountains of South America are home to 99 percent of tropical glaciers ­— permanent rivers of ice at high enough elevations not to be affected by the types of balmy temperatures usually associated with the tropics.
+
+The Andes are also home to the world’s highest volcano when measured above sea level, Ojos del Salado on the Chile-Argentina border, which reaches 22,615 feet (6,893 m), and more than 50 volcanoes that reach 19,685 feet (6,000 m).
+
+Machu Picchu was an Incan retreat for its emperor. 
+
+Who and What Lives There
+
+A number of cities have sprung up along the Andes, including Huaraz and Cuzco in Peru; Quito and Baños in Ecuador; La Paz, one of the two capitals of Bolivia; Bogata and Medellin in Columbia; and Pucon and Puerto Natales in Chile.
+
+There is not a lot of evidence of early human life in the Andes, with the oldest human remains being an average of 10,000 to 12,000 years old; although scientists believe there was human habitation prior to that time. The lack of oxygen at high altitudes and rough terrain made it hard for people survive. The mountain shepherds have survived at higher altitudes of 17,000 feet on a permanent basis because their bodies have been able to adapt, including changes to the makeup of their cells.
+
+The Incas managed to establish themselves in the Peruvian Andes in the 15th century, only to be wiped out by civil wars and disease from European invaders to which they had not developed immunities. Their existence was largely undiscovered, save for some local residents, until July 24, 1911, when the locals led Yale University professor Hiram Bingham to Machu Picchu, at an elevation of 7,710 feet (2,350 m) in the Peru portion of the mountain range.
+
+Because it was relatively undisturbed, Machu Picchu provided a glimpse into the life of the Incas and scientists believe it served as the palace compound of Pachacuti Inca Yupanqui, who reigned from about 1438 to 1471. It now serves as a major tourist destination.
+
+While Bingham thought he had stumbled upon the Vilcabamba, or lost city of the Incas, when he came upon Machu Picchu, he would later find the so-called lost city when he discovered another ruin, Espíritu Pampa.
+
+To survive in the Andes, animals have to be rugged, agile and be able to keep warm. One of the most well-known animal inhabitants of the Andes is the chinchilla. Their fur gets thicker in the colder, higher altitudes and their ability to jump and climb serves as a protection from predators. Other animals include llamas and alpacas; the Andean condor, the largest vulture in South America; and the vicuña, a member of the camel family.
+
+The Andes has a rich plant population and is home to about 30,000 various species of plants. The Andes are dotted with Cinchona pubescens trees, which produce quinine, coveted as a treatment for malaria. In the high altitudes of the Colombia, Ecuador, Peru, Bolivia and Chile segments of the range, there are also forests of Polylepis, a group of tree species that have distinctive gnarled shapes. They are members of the rose family.
+
+</textarea> <img src="http://www.davestravelcorner.com/wp-blog/wp-content/uploads/2006/02/Andean-Sunset.jpg" style="height:23%;width:relative"> <img src="http://www.circleofblue.org/wp-content/uploads/2010/04/Bolivian-Andes-290.jpg" style="height:23%;width:relative"> <img src="http://www.natureserve.org/sites/default/files/paramo_and_espeletia_near_belen_colombia_feb_2010.jpg" style="height:23%;width:relative"> <img src="https://s-media-cache-ak0.pinimg.com/736x/f1/36/d6/f136d6c02fd3be082cf95defaade853e.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Andes">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
