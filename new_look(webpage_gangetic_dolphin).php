7a16af14c4ba20d85a62a3ddeed00d74cd5ccb20
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:39:33 +0530

    Create new_look(webpage_gangetic_dolphin).php

---
+++ new_look(webpage_gangetic_dolphin).php
@@ -0,0 +1,208 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Animals</TITLE>
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Animals</z>
+</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					height:90px;
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<body>
+<H1 class="class1">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:lightblue;width:90%;padding:3%"><img src="http://assets.worldwildlife.org/photos/42/images/featured_story/Pink_river_dolphin_circle_image_(c)_naturepl.com_Mark_Carwardine_WWF.jpg?1345512990" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.livescience.com/images/i/000/024/530/original/ganges-river-dolphin.jpeg?interpolation=lanczos-none&fit=inside%7C660:*" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.examveda.com/images/theory-image/current-affairs/2016/june/1465358568-river-dolphin.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://1.bp.blogspot.com/-3vUrH4mf2qg/UcL81OluxtI/AAAAAAAAADM/Um76J-5HLZM/s1600/boliviariverdolphin1.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.riverdolphintrust.org/wp-content/uploads/2015/07/t-156-copy_20120414075654.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1>
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class5">
+Gangetic Dolphin
+</H1>
+</td>
+<p>
+<!--<pre>-->
+<div style="background-color:pink;padding:5px;border-radius:5px;overflow:hidden"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%">The South Asian river dolphin (Platanista gangetica) is a freshwater or river dolphin found in India, Bangladesh, Nepal and Pakistan which is split into two subspecies, the Ganges river dolphin (P. g. gangetica) and Indus river dolphin (P. g. minor).[2] The Ganges river dolphin is primarily found in the Ganges and Brahmaputra Rivers and their tributaries in Bangladesh, India and Nepal, while the Indus river dolphin is found in the Indus River in Pakistan and its Beas and Sutlej tributaries. From the 1970s until 1998, they were regarded as separate species; however, in 1998, their classification was changed from two separate species to subspecies of a single species (see taxonomy below).
+
+The Ganges river dolphin has been recognized by the government of India as its National Aquatic Animal.[3] The Indus river dolphin has been named as the National Mammal of Pakistan.[4] Further, the Gangetic river dolphin has been elected to be the city animal of the Indian city of Guwahati.[5]
+
+Taxonomy[edit]
+The species was described by two separate authors, Lebeck and Roxburgh, in 1801, and it is unclear to whom the original description should be ascribed.[6] Until the 1970s, the South Asian river dolphin was regarded as a single species. The two subspecies are geographically separate and have not interbred for many hundreds if not thousands of years. Based on differences in skull structure, vertebrae and lipid composition scientists declared the two populations as separate species in the early 1970s.[7] In 1998, the results of these studies were questioned and the classification reverted to the pre-1970 consensus of a single species containing two subspecies until the taxonomy could be resolved using modern techniques such as molecular sequencing. The latest analyses of mitochondrial DNA of the two subspecies did not display the variances needed to support their classification as separate species.[8] Thus, at present, this one species with two subspecies is recognized in the genus Platanista, the P. g. gangetica (Ganges river dolphin) and the P. g. minor (Indus river dolphin).[9]
+Synonyms
+1.blind river dolphin, side-swimming dolphin
+2.Ganges subspecies: Gangetic dolphin, Ganges susu,[10] shushuk
+3.Indus subspecies: bhulan, Indus dolphin, Indus blind dolphin
+
+<<--------------------------------------Description---------------------------------------->>
+
+The South Asian river dolphin has the long, pointed nose characteristic of all river dolphins. Its teeth are visible in both the upper and lower jaws even when the mouth is closed. The teeth of young animals are almost an inch long, thin and curved; however, as animals age, the teeth undergo considerable changes and in mature adults become square, bony, flat disks. The snout thickens towards its end. The species does not have a crystalline eye lens, rendering it effectively blind, although it may still be able to detect the intensity and direction of light. Navigation and hunting are carried out using echolocation.[11] They are unique among cetaceans in that they swim on their sides.[12] The body is a brownish color and stocky at the middle. The species has only a small, triangular lump in the place of a dorsal fin. The flippers and tail are thin and large in relation to the body size, which is about 2-2.2 meters in males and 2.4-2.6 m in females. The oldest recorded animal was a 28-year-old male, 199 cm in length.[13] Mature females are larger than males. Sexual dimorphism is expressed after females reach about 150 cm (59 in); the female rostrum continues to grow after the male rostrum stops growing, eventually reaching approximately 20 cm (7.9 in) longer.
+
+Distribution and habitat[edit]
+The South Asian river dolphins are native to the freshwater river systems located in Nepal, India, Bangladesh, and Pakistan.[1] They can be most commonly found in water with high abundance of prey and reduced flow.[11]
+The Ganges subspecies (P. g. gangetica) can be found along the Ganges-Brahmaputra-Meghna and Karnaphuli-Sangu river systems of Bangladesh and India, although its range formerly extended to Nepal.[11] A small subpopulation can be still found on the Ghaghara River and possibly the Sapta Kosi River. The majority of the Indus subspecies (P. g. minor) is located between the Sukkur and Guddu barrage in the Sind Province of Pakistan.[11] Two smaller subpopulations have also been recorded in the Punjab and Khyber Pakhtunkhwa Provinces. Since the two river systems are not connected in any way, it is a mystery how these sub-species ended up in the two rivers. It is improbable that the river dolphins made it from one river to another through the sea route since the two estuaries are very far apart. According to Land of the Seven Rivers: A Brief History of India's Geography by Sanjeev Sanyal, a likely explanation is that several north Indian rivers like the Sutlej and Yamuna changed their path in ancient times and carried the dolphins with them.[14]
+</textarea> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Cetacea_range_map_South_Asian_river_dolphin.png/220px-Cetacea_range_map_South_Asian_river_dolphin.png" style="height:23%;width:relative"> <img src="https://lh3.googleusercontent.com/-kCvRlj5XHn8/V0iYwer08HI/AAAAAAAAFOA/j2MbfY2PplgkfOabaFOtmhAumDsCJgwgw/w426-h320/16%2B-%2B1" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/South_Asian_river_dolphin">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</BODY>
+</HTML>
