b2e3178852799ea6fd6c5fc49a7e18a4e7d3ff53
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:42:53 +0530

    Create new_look(webpage_sea_otter).php

---
+++ new_look(webpage_sea_otter).php
@@ -0,0 +1,210 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Animals</TITLE>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					height:90px;
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Animals</z>
+</p>
+
+<H1 class="class1">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:violet;width:90%;padding:3%"><img src="http://thrutcher.com/wp-content/uploads/2015/12/sea-otters.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://www.montereybayaquarium.org/-/m/images/hero/sea-otter-exhibit.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://www.kimballstock.com/pix/MAM/09/MAM-09-HB0012-01P.JPG" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.gannett-cdn.com/-mm-/c22b61ce8d10eca2938b8a63ee112e055920a874/c=120-133-569-471&r=x404&c=534x401/local/-/media/2015/12/22/USATODAY/USATODAY/635863654553327069-Screen-Shot-2015-12-22-at-7.17.06-AM.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://a-z-animals.com/media/animals/images/470x370/otter2.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1>
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class3">
+Sea Otter
+</H1>
+</td>
+<p>
+<!--<pre>-->
+<div style="background-color:brown;padding:5px;border-radius:5px;overflow:hidden"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%">Not to be confused with the marine otter or European otter (sometimes called sea otter). "Enhydra" redirects here. For the free server software, see Enhydra Server. For other uses, see Sea otter (disambiguation).
+
+The sea otter (Enhydra lutris) is a marine mammal native to the coasts of the northern and eastern North Pacific Ocean. Adult sea otters typically weigh between 14 and 45 kg (31 and 99 lb), making them the heaviest members of the weasel family, but among the smallest marine mammals. Unlike most marine mammals, the sea otter's primary form of insulation is an exceptionally thick coat of fur, the densest in the animal kingdom. Although it can walk on land, the sea otter lives mostly in the ocean.
+The sea otter inhabits offshore environments, where it dives to the sea floor to forage. It preys mostly on marine invertebrates such as sea urchins, various molluscs and crustaceans, and some species of fish. Its foraging and eating habits are noteworthy in several respects. First, its use of rocks to dislodge prey and to open shells makes it one of the few mammal species to use tools. In most of its range, it is a keystone species, controlling sea urchin populations which would otherwise inflict extensive damage to kelp forest ecosystems. Its diet includes prey species that are also valued by humans as food, leading to conflicts between sea otters and fisheries.
+Sea otters, whose numbers were once estimated at 150,000–300,000, were hunted extensively for their fur between 1741 and 1911, and the world population fell to 1,000–2,000 individuals living in a fraction of their historic range.[3] A subsequent international ban on hunting, conservation efforts, and reintroduction programs into previously populated areas have contributed to numbers rebounding, and the species now occupies about two-thirds of its former range. The recovery of the sea otter is considered an important success in marine conservation, although populations in the Aleutian Islands and California have recently declined or have plateaued at depressed levels. For these reasons, the sea otter remains classified as an endangered species.
+
+<<--------------------------------------Description---------------------------------------->>
+
+Taxonomy
+The first scientific description of the sea otter is contained in the field notes of Georg Steller from 1751, and the species was described by Linnaeus in his Systema Naturae of 1758.[5] Originally named Lutra marina, it underwent numerous name changes before being accepted as Enhydra lutris in 1922.[6] The generic name Enhydra, derives from the Ancient Greek en/εν "in" and hydra/ύδρα "water",[7] meaning "in the water", and the Latin word lutris, meaning "otter".[8]
+The sea otter was formerly sometimes referred to as the "sea beaver",[9] being the marine fur-bearer similar in commercial value to the terrestrial beaver. Rodents (of which the beaver is one) are not closely related to otters, which are carnivores. It is not to be confused with the marine otter, a rare otter species native to the southern west coast of South America. A number of other otter species, while predominantly living in fresh water, are commonly found in marine coastal habitats. The extinct sea mink of northeast North America is another mustelid that had adapted to a marine environment.
+
+Evolution[edit]
+The sea otter is the heaviest (the giant otter is longer, but significantly slimmer) member of the family Mustelidae,[10] a diverse group that includes the 13 otter species and terrestrial animals such as weasels, badgers, and minks. It is unique among the mustelids in not making dens or burrows, in having no functional anal scent glands,[11] and in being able to live its entire life without leaving the water.[12] The only member of the genus Enhydra, the sea otter is so different from other mustelid species that, as recently as 1982, some scientists believed it was more closely related to the earless seals.[13] Genetic analysis indicates the sea otter and its closest extant relatives, which include the African speckle-throated otter, European otter, African clawless otter and oriental small-clawed otter, shared an ancestor approximately 5 million years ago (Mya).[14]
+Fossil evidence indicates the Enhydra lineage became isolated in the North Pacific approximately 2 Mya, giving rise to the now-extinct Enhydra macrodonta and the modern sea otter, Enhydra lutris.[6] The sea otter evolved initially in northern Hokkaidō and Russia, and then spread east to the Aleutian Islands, mainland Alaska, and down the North American coast.[15] In comparison to cetaceans, sirenians, and pinnipeds, which entered the water approximately 50, 40, and 20 Mya, respectively, the sea otter is a relative newcomer to a marine existence.[16] In some respects, though, the sea otter is more fully adapted to water than pinnipeds, which must haul out on land or ice to give birth.[17]
+
+</textarea> <img src="https://neyssasaryink.files.wordpress.com/2014/02/otter-ranger-map.png" style="height:23%;width:relative"> <img src="https://soulballadeer.files.wordpress.com/2012/12/sea-otter-below-the-surfac.jpg?w=350&h=315" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Sea_otter">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
