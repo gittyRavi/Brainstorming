c66e6e5971013b1019c67632939e990193eb0003
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:30:31 +0530

    Create new_look(webpage_green_sea_turtle).php

---
+++ new_look(webpage_green_sea_turtle).php
@@ -0,0 +1,209 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Animals</TITLE>
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Animals</z>
+</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					height:90px;
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<body>
+<H1 class="class1">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:violet;width:90%;padding:3%"><img src="http://animalfactguide.com/wp-content/uploads/2013/01/greenturtle.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.factzoo.com/sites/all/img/reptiles/turtle/green-turtle-baby.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://travel.vesti.ru/pictures/mbig/380/7.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Total_internal_reflection_of_Chelonia_mydas.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://userscontent2.emaze.com/images/1a80a328-b850-4c17-90aa-a9a1f5733f26/2692b938-9edc-4cf3-b762-09799ed62690.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1>
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class3">
+Green Sea Turtle
+</H1>
+</td>
+<p>
+<!--<pre>-->
+<div style="background-color:palegreen;padding:5px;border-radius:5px;overflow:hidden"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%">For the fictional superhero, see The Green Turtle.
+
+The green sea turtle (Chelonia mydas), also known as the green turtle, black (sea) turtle, or Pacific green turtle,[3] is a large sea turtle of the family Cheloniidae. It is the only species in the genus Chelonia.[4] Its range extends throughout tropical and subtropical seas around the world, with two distinct populations in the Atlantic and Pacific Oceans, but it is also found in the Indian Ocean.[5][6] The common name comes from the usually green fat found beneath its carapace; these turtles' shells are olive to black.
+
+This sea turtle's dorsoventrally flattened body is covered by a large, teardrop-shaped carapace; it has a pair of large, paddle-like flippers. It is usually lightly colored, although in the eastern Pacific populations parts of the carapace can be almost black. Unlike other members of its family, such as the hawksbill sea turtle, C. mydas is mostly herbivorous. The adults usually inhabit shallow lagoons, feeding mostly on various species of seagrasses.[7] The turtles bite off the tips of the blades of seagrass, which keeps the grass healthy.
+Like other sea turtles, green sea turtles migrate long distances between feeding grounds and hatching beaches. Many islands worldwide are known as Turtle Island due to green sea turtles nesting on their beaches. Females crawl out on beaches, dig nests and lay eggs during the night. Later, hatchlings emerge and scramble into the water. Those that reach maturity may live to eighty years in the wild.[5]
+C. mydas is listed as endangered by the IUCN and CITES and is protected from exploitation in most countries.[8] It is illegal to collect, harm or kill them. In addition, many countries have laws and ordinances to protect nesting areas. However, turtles are still in danger due to human activity. In some countries, turtles and their eggs are hunted for food. Pollution indirectly harms turtles at both population and individual scales. Many turtles die after being caught in fishing nets. Also, real estate development often causes habitat loss by eliminating nesting beaches.
+
+Taxonomy[edit]
+The green sea turtle is a member of the tribe Chelonini. A 1993 study clarified the status of genus Chelonia with respect to the other marine turtles. The carnivorous Eretmochelys (hawksbill), Caretta (loggerhead) and Lepidochelys (Ridley) were assigned to the tribe Carettini. Herbivorous Chelonia warranted their status as a genus, while Natator (flatback) was further removed from the other genera than previously believed.[9]
+The species was originally described by Linnaeus in 1758 as Testudo mydas.[10] In 1868, Marie Firmin Bocourt named a particular species of sea turtle Chelonia agassizii.[11][12] This "species" was referred to as the "black sea turtle".[13] Later research determined Bocourt's "black sea turtle" was not genetically distinct from C. mydas, and thus taxonomically not a separate species.[14] These two "species" were then united as Chelonia mydas and populations were given subspecies status: C. mydas mydas referred to the originally described population, while C. mydas agassizi referred only to the Pacific population known as the Galápagos green turtle.[15][16] This subdivision was later determined to be invalid and all species members were then designated Chelonia mydas.[4] The oft-mentioned name C. agassizi remains an invalid junior synonym of C. mydas. Other authorities claim that the two populations are phenotypically distinct and treat the East Pacific form as a full species.[17]
+The species' common name does not derive from any particular green external coloration of the turtle. Its name comes from the greenish color of the turtles' fat, which is only found in a layer between their inner organs and their shell.[18] As a species found worldwide, the green turtle is called differently in some languages and dialects. In the Hawaiian language, honu[19] is used to refer to this species.[20] In Hawaii, the green sea turtle is referred to as “honu” and is a symbol of good luck and longevity.[21]
+
+<<--------------------------------------Description---------------------------------------->>
+
+Its appearance is that of a typical sea turtle. C. mydas has a dorsoventrally flattened body, a beaked head at the end of a short neck, and paddle-like arms well-adapted for swimming.[22] Adult green turtles grow to 1.5 metres (5 ft) long.[23] The average weight of mature individuals is 68–190 kg (150–419 lb) and the average carapace length is 78–112 cm (31–44 in).[24] Exceptional specimens can weigh 315 kg (694 lb) or even more, with the largest known C. mydas having weighed 395 kg (871 lb) and measured 153 cm (60 in) in carapace length.[25]
+Anatomically, a few characteristics distinguish the green turtle from the other members of its family. Unlike the closely related hawksbill turtle, the green turtle's snout is very short and its beak is unhooked. The neck cannot be pulled into the shell.[26] The sheath of the turtle's upper jaw possesses a denticulated edge, while its lower jaw has stronger, serrated, more defined denticulation. The dorsal surface of the turtle's head has a single pair of prefrontal scales. Its carapace is composed of five central scutes flanked by four pairs of lateral scutes. Underneath, the green turtle has four pairs of inframarginal scutes covering the area between the turtle's plastron and its shell. Mature C. mydas front appendages have only a single claw (as opposed to the hawksbill's two), although a second claw is sometimes prominent in young specimens.[27]
+The carapace of the turtle has various color patterns that change over time. Hatchlings of Chelonia mydas, like those of other marine turtles, have mostly black carapaces and light-colored plastrons. Carapaces of juveniles turn dark brown to olive, while those of mature adults are either entirely brown, spotted or marbled with variegated rays. Underneath, the turtle's plastron is hued yellow. C. mydas limbs are dark-colored and lined with yellow, and are usually marked with a large dark brown spot in the center of each appendage.[5][28]
+
+</textarea> <img src="http://animalfactguide.com/wp-content/uploads/2013/01/worldmap_greenturtle.png" style="height:23%;width:relative"> <img src="https://designeranimals.wikispaces.com/file/view/green-seaturtle-5.jpg/149105733/495x312/green-seaturtle-5.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Green_sea_turtle">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</BODY>
+</HTML>
