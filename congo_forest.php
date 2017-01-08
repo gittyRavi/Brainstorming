1062654c8a3e161f4508dc36006e0426596be483
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:50:06 +0530

    Create congo_forest.php

---
+++ congo_forest.php
@@ -0,0 +1,239 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("https://d.wattpad.com/story_parts/310568784/images/14752ef6faf2c93861788380621.jpg");
+						background-width:device-width;
+						background-position: center center;
+						background-size:100%;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class6" style="opacity:0.8;font-weight:bold;color:grey;">Region-(Central Africa)-Forest</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:grey;width:90%;padding:3%;background: rgb(255, 255, 255);background: rgba(255, 255, 255, 0.3);"><img src="http://images.wisegeek.com/misty-rainforest.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.ciel-voyages.ro/Files/Pictures/Images/republica-democrata-congo-junglele-africane-13194.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://userscontent2.emaze.com/images/e9c97043-48ed-4193-80ad-f85a391f2def/ff6df4784b7d2fcfccc61cce664ff9f3.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://i.telegraph.co.uk/multimedia/archive/03469/Large-group-of-ele_3469124b.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://www.awf.org/sites/default/files/media/images/Bili-Uele%20Jumpstart_0.jpg" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class2" style="opacity:0.8;color:green;">
+Congo Rainforest
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div style="background-color:#CCCAFA;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:white;background-color:grey;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+Introduction
+On this page of Interesting Africa facts we list many facts about the amazing Congo Rainforest (also called the Congo Basin Rainforest). There is information on where it is located, what the climate is like, who lives there, the animals that live there, and the plant life. These facts help explain why this rainforest is one of the most incredible African landforms. This information is written for both kids and adults.
+Congo Rainforest General Facts
+The rainforest is located in central Africa.
+It spans approximately 1.5 million square miles.
+It's the world's second largest rainforest (only the Amazon is larger).
+Most of the Congo Rainforest is in the Democratic Republic of Congo (DRC); however there are also significant areas in Cameroon, Gabon, Equatorial Guinea, the Republic of Congo, and the Central African Republic.
+The most famous Congo rainforest people are the Pygmies. The average height of Pygmy men is just 4 feet 10 inches (1.45 meters). The average height of Pygmy women is just 4 feet 1 inch (1.33 meters).
+Commercial logging and expansion of farming has made the Congo rainforest one of the most endangered ecosystems in the world.
+Five of the national parks in the Congo Rainforest are UN World Heritage Sites (meaning they have special significance to the world).
+The Congo River, which is the second largest river in the world, flows through the rainforest.
+The Congo is very rainy. The average rainfall is a little over 58 inches (147 centimeters) per year.
+The climate of the Congo is warm and wet with an average temperature of 77 degrees Fahrenheit.
+Congo Rainforest Animal Facts
+The Congo rainforest is teeming with life, it is home to approximately 450 species of mammals, 300 reptile species, 200 amphibian species, and over 1,000 birds species. The following list of facts describes some of these amazing animals.
+The Congo rainforest is the only place in the world where all three subspecies of gorillas can be found. They are the mountain gorilla, lowland gorilla, and the eastern lowland gorilla.
+Bonobos are found only in the Congo rainforest, they are the closest relatives to human beings.
+The African forest elephant can be found in the Congo rainforest. It is smaller than the savanna elephant and has downward-projecting tusks.
+The okapi is an animal that looks like a cross between a giraffe and a horse. In fact it is often called forest giraffe. It is brown and has white striped legs. It is found only in the rainforest of the Democratic Republic of Congo (DRC).
+Black Colobus monkeys can be found living in the high canopies of the Congo rainforest. They are well known for their amazing jumping skills. They are one of the most endangered monkeys in Africa.
+Congo Rainforest Plant Facts
+The rainforests of the Congo is home to more than 11,000 different species of plants. Over one thousand of these species are unique to the Congo rainforest. Below we list a few of these species and some interesting facts about these rainforest plants.
+The Congo rainforest vegetation is so dense that many parts of it have never been seen by humans.
+In the most dense parts of the rainforest only 1% of the sunlight reaches the ground.
+Cancer Institutes have identified approximately 1,400 rainforest plants which could potentially be used to fight cancer.
+Lianas, which look a lot like vines, are common in the Congo rainforest. They can grow up to three thousand feet long (914 meters).
+Teak trees are common in the rainforest of the Congo. They can grow up to 50 meters (154 feet) high. These trees are in high demand as a source of wood and one of the main reasons for the alarming rate of deforestation in the forest.
+</textarea> <img src="http://images.nationalgeographic.com/wpf/media-live/photos/000/238/cache/rain-forest-canoe-congo_23897_600x450.jpg" style="height:23%;width:relative"> <img src="http://spi4uk.itvnet.lv/upload2/articles/69/691822/images/Vissvissviss-4.jpg" style="height:23%;width:relative"> <img src="http://lol54.ru/uploads/posts/2010-12/thumbs/1292837238_1291814048_national-geographic59.jpg" style="height:23%;width:relative"> <img src="https://s-media-cache-ak0.pinimg.com/564x/d1/ed/4b/d1ed4b744b5f9fc3c9e3fe764382a3fe.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Congolian_forests">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
