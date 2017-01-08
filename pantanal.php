b6169cbc6be15cfa6a78b018059e11d80396cb4d
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:47:58 +0530

    Create pantanal.php

---
+++ pantanal.php
@@ -0,0 +1,238 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("http://i1.wp.com/mariaeunicesousa.com/wp-content/uploads/2015/06/mike-bueno.jpg");
+						background-width:device-width;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class6" style="opacity:0.8;color:grey;">Region-(Americas)-Swamp</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:#8B8B59;width:90%;padding:3%;background: rgb(139, 139, 89);background: rgba(139, 139, 89, 0.6);"><img src="http://s016.radikal.ru/i337/1408/bf/5bd7b6055a5e.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://scontent.cdninstagram.com/t51.2885-15/e35/13658336_593725337476451_844095332_n.jpg?ig_cache_key=MTI5MTI1ODE4NTUzMjkyNjM3Nw%3D%3D.2" alt="Image" style="width:19.2%;height:relative;"><img src="http://3.bp.blogspot.com/-9MkoIlMoXgU/VRz2Rw4CcLI/AAAAAAAAI9M/EaJEgZc1HeI/s1600/Flood%2Bplains%2Bin%2Bthe%2BPantanal,%2BBrazil%2B20150402.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://zooterra.files.wordpress.com/2010/04/jacare-do-pantanal-caiman-yacare-11.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="https://www.naturalworldsafaris.com/~/media/images/destinations/brazil/brazil-pantanal.ashx" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class7" style="opacity:0.8;color:brown;">
+Pantanal
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div style="background-color:#15C69C;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:#31983E;background-color:#DAF7A6;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+The Pantanal
+
+With a total area of almost 195 000 square kilometres (or 75 000 square miles), the Pantanal is the largest wetland in the world. The vast majority of it is in the state of Mato Grosso do Sul, but it also extends into Mato Grosso and the separate countries of Paraguay and Bolivia, which neighbour Brazil.
+
+As such a vast tropical wetland, the Pantanal is a very precious resource of Brazil, home to an array of plant- and animal species. In fact, up to 12 sub-regional ecosystems have been identified within the Pantanal, each of which has its own unique identity and characteristics.
+
+Image of the Pantanal wetlands during the dry season. County of Corumbá, Mato Grosso do Sul, Brazil. UNESCO World Nature Heritage site and Biosphere Reserve.
+Pantanal wetlands during the dry season. County of Corumbá, Mato Grosso do Sul, Brazil. UNESCO World Nature Heritage site and Biosphere Reserve.
+
+Because about 80% of the wetlands are submerged during the wet season, the species here include aquatic ones, making it an even more diverse and fascinating destination to consider. The water can rise by up to five metres during the wet season. The daily highs average an annual mean of 25 degrees Celsius.
+
+However, summer days have been known to soar to a scorching 40 degrees, while winter nights can plummet to zero.
+
+Tourists that are interested in the ecosystems of the Pantanal will be well rewarded by the sheer diversity to be found here. The biomes here (which include semi-arid woodland, tropical Amazonian rainforest and savannah) are home to approximately 3 500 known plant species, with more discovered and identified by scientists on a regular basis.
+
+Because some of the wetlands suffer from drought during the dry seasons, certain vegetation experiences water-stress.
+
+A plethora of animal species can be found in the Pantanal. There is estimated to be about 1 000 bird species, 300 mammals and 9 000 invertebrates, in addition to countless fascinating insects and other species. Some of the very rare and / or endangered animal species include:
+
+•	Marsh Deer				•	Giant River Otter
+•	Hyacinth Macaw		•	Crowned Solitary Eagle
+•	Jaguar					•	Maned Wolf
+•	Bush Dog				•	Capybara
+•	South American Tapir	•	Giant Anteater
+•	Yacare Caiman
+
+The Pantanal’s dry season extends from April to October of each year. During this time, it is possible to access the region by road.
+
+</textarea> <img src="https://cecifontainecomblog.files.wordpress.com/2016/04/butterflies-resting-on-a-caiman-in-the-pantanal-brazil.jpg?w=640" style="height:23%;width:relative"> <img src="https://tabimo.jp/imgs/tabimo/raw/articles/295/20_s6y7d.jpg" style="height:23%;width:relative"> <img src="https://www.naturalworldsafaris.com/~/media/images/destinations/brazil/brazil-wildlife-bird-toucan.ashx" style="height:23%;width:relative"> <img src="http://2.bp.blogspot.com/-w1Wm3ne25WI/VjrToi0rVEI/AAAAAAAAATg/adabuJ7L4J0/s1600/jaguar.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Pantanal">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
