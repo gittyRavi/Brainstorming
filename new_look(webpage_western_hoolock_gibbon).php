c0e422c37d38b50854b2f6e87451d74121a942b9
gittyRavi <rairavisaxena@gmail.com>
2017-01-08 05:31:34 +0530

    Create new_look(webpage_western_hoolock_gibbon).php

---
+++ new_look(webpage_western_hoolock_gibbon).php
@@ -0,0 +1,212 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+<link rel="icon" href="icon.png" type="image/png" sizes="32x32">
+
+<TITLE>World . Animals</TITLE>
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">My Artworks</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Animals</z>
+</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					height:90px;
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<body>
+<H1 class="class1">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:purple;width:90%;padding:3%"><img src="http://www.thehindu.com/multimedia/dynamic/00285/ZOO_5_285332g.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://2.bp.blogspot.com/-ytszMJTaY3g/VQkkE_QLvqI/AAAAAAAAFf4/MSj-TsjiXoQ/s1600/Western%2BHoolock%2BGibbon%2Bindia.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.wildworldindia.com/wp-content/uploads/AS_0000010654-786593168-O-copy.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/10/1-269.jpg" alt="Image" style="width:19.2%;height:relative;"><img src="http://cdn.banglatribune.com/contents/cache/images/800x0x1/uploads/media/2016/06/22/bcaa09ba79928ee7a7273b128fb5504d-5769bcaae4e8a.JPG" alt="Image" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1>
+ <H5 style='color:white'>Hello World!! this is php</H5></H1>
+<td>
+<H1 class="class7">
+Western Hoolock Gibbon
+</H1>
+</td>
+<p>
+<!--<pre>-->
+<div style="background-color:brown;padding:5px;border-radius:5px;overflow:hidden"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%">The hoolock gibbons are two primate species of genus Hoolock in the gibbon family, Hylobatidae, native to eastern Bangladesh, Northeast India and Southwest China.
+
+Hoolocks are the second-largest of the gibbons, after the siamang. They reach a size of 60 to 90 cm and weigh 6 to 9 kg. The sexes are about the same size, but they differ considerably in coloration: males are black-colored with remarkable white brows, while females have a grey-brown fur, which is darker at the chest and neck. White rings around their eyes and mouths give their faces a mask-like appearance.
+In northeast India, the hoolock is found south of Brahmaputra and east of the Dibang Rivers.[3] Its range extends into seven states covering Arunachal Pradesh, Assam, Manipur, Meghalaya, Mizoram, Nagaland, and Tripura (The seven northeastern states of India).[4][5]
+Like the other gibbons, they are diurnal and arboreal, brachiating through the trees with their long arms. They live together in monogamous pairs, which stake out a territory. Their calls serve to locate family members and ward off other gibbons from their territory. Their diet consists mainly of fruits, insects and leaves.
+Young hoolocks are born after a seven-month gestation, with milky white or buff-colored hair. After about six months, the hair of males will darken and turn black, while the female hair remains buff-colored throughout her life. After eight to 9 years, they are fully mature and their fur reaches its final coloration. Their life expectancy in the wild is about 25 years.
+
+The classification of this gibbon has changed several times in the past few years. Classically, all gibbons were classified in the genus Hylobates, with the exception of the siamang. After some studies, the genus was divided into three subgenera (including the siamang's Symphalangus), and then into four (recognizing Bunopithecus as the hoolock subgenus distinct from other gibbon subgenera). These four subgenera were elevated to full genus status. However, the type species for Bunopithecus is Bunopithecus sericus, an extinct gibbon or gibbon-like ape from Sichuan, China. Very recent investigations have shown the hoolocks are not closely related to B. sericus, so have been placed in their own genus, Hoolock. In the process, the two subspecies of hoolocks have been raised to species level.[1][2] A new subspecies has been described recently from northeastern India, which has been named Mishmi Hills hoolock, Hoolock hoolock mishmiensis.[6]
+The two species of hoolock are:[2]
+1.Western hoolock gibbon, Hoolock hoolock
+2.Eastern hoolock gibbon, Hoolock leuconedys
+
+<<--------------------------------------Description---------------------------------------->>
+
+Classification[edit]
+Mootnick and Groves[3] stated that hoolock gibbons do not belong in the genus Bunopithecus, and placed them in a new genus, Hoolock. This genus was argued to contain two distinct species which were previously thought to be subspecies: Hoolock hoolock and Hoolock leuconedys which were later have found that the 2 species have a bigger difference to that of white handed gibbons than bonobos to chimpanzees.[4]
+
+Vocalisation[edit]
+Like other gibbons, hoolock gibbon pairs produce a loud, elaborate song, usually sung as a duet from the forest canopy, in which younger individuals of the family group may join in. The song includes an introductory sequence, an organising sequence, and a great call sequence, with the male also contributing to the latter (unlike in some other gibbon species).[5]
+
+Habitat[edit]
+In India and Bangladesh it is found where there is contiguous canopy, broad-leaved, wet evergreen and semi-evergreen forests dipterocarpus forest often mountainous. The species is an important seed disperser; its diet includes mostly ripe fruits, with some flowers, leaves and shoots.[citation needed]
+</textarea> <img src="http://pin.primate.wisc.edu/fs/sheets/maps/hoolock_range_large.gif" style="height:23%;width:relative"> <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Gibbon_Amiens_26872.jpg" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<a href="yt-js-player.html" target="_blank"><img src="youtube-logo-js-player.png" style="width:10%;height:relative;"></img></a>
+</div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy R.S.
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Western_hoolock_gibbon">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</BODY>
+</HTML>
