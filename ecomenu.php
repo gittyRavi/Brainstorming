434cc16b5ca837244827042f3f14f1a810c7cb04
gittyRavi <rairavisaxena@gmail.com>
2017-01-09 10:25:47 +0530

    Update ecomenu.php

--- ecomenu.php
+++ ecomenu.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="A New Website.">
-<meta name="author" content="Ravi Saxena">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Language" content="en">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="A New Website.">
+  <meta name="author" content="Ravi Saxena">
 <link rel="icon" href="icon.png" type="image/png" sizes="32x32">
-<title>Music & Videos</title>
+<title>ECO</title>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -141,10 +143,10 @@ $(document).ready(function()
     <div class="navbar-header">
       <a class="navbar-brand" href="#" style="color:white;"><h5>Search The Content</h5></a>
     </div><br/><br/>
-	<button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="home">HOME</button>
-	<button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id1">MENU</button>
-    <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id2">ARCHIVE</button>
-    <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id3">My Artworks</button>
+	<button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="home"><span class="glyphicon glyphicon-home"></span> HOME</button>
+	<button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id1"><span class="glyphicon glyphicon-text-background"></span> MENU</button>
+    <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id2"><span class="glyphicon glyphicon-tags"></span> ARCHIVE</button>
+    <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id3"><span class="glyphicon glyphicon-user"></span> My Artworks</button>
   </div>
 </nav>
 
@@ -305,7 +307,7 @@ Arctic
 
 <footer class="container-fluid text-center" style="background-color: #004d40;opacity:0.9;color:white;padding:1%;bottom:0px;position:relative;width:100%;">
   <center>
-  <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id4">Contact</button>
+  <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id4"><span class="glyphicon glyphicon-envelope"></span> Contact</button>
   <h6>Dated 9/12/16.</h6>
   </center>
 </footer>
