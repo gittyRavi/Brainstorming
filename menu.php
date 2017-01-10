aafeb5e41827cc414c3fd83358f22540f513735c
gittyRavi <rairavisaxena@gmail.com>
2017-01-10 14:47:01 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -13,7 +13,7 @@
   <link rel="icon" href="icon.png" type="image/png" sizes="32x32">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"type='text/css'>
+  <link href="https://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"type='text/css'>
   <link rel="stylesheet" href="sidebar.css">
   <style>
   @media screen and (min-width: 431px) 
