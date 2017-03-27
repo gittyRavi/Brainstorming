47485130dfe43e163b17ed213c0c00b8a72167ae
রবি সাক্সেনা /  Рави Саксена <gittyRavi@users.noreply.github.com>
2017-03-28 02:27:11 +0530

    Add files via upload

---
+++ ABuJa.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+  // Fakes the loading setting a timeout
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 3000);
+ 
+});
+  
+ /* Thanks to:
+  http://www.cantinanegrar.it/
+*/
