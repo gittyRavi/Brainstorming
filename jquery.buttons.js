5820a555af13fa58d19c757690a98f5b4c4f5837
রবি সাক্সেনা /  Рави Саксена <gittyRavi@users.noreply.github.com>
2017-07-06 23:00:45 +0530

    Update jquery.buttons.js

--- jquery.buttons.js
+++ jquery.buttons.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 	$("#home").click(function(){
-		window.location = "index.html";
+		window.location = "https://brainstorming.000webhostapp.com/index.html";		//index.html
 		return false;
 	});
 
 	$("#id1").click(function(){
-		window.location = "menu.php";
+		window.location = "https://brainstorming.000webhostapp.com/menu.php";		//menu.php
 		return false;
 	});
 	
 	$("#id2").click(function(){
-		window.location = "archive.php";
+		window.location = "https://brainstorming.000webhostapp.com/archive.php";	//archive.php
 		return false;
 	});
 	
 	$("#id3").click(function(){
-		window.location = "main_info.php";
+		window.location = "https://brainstorming.000webhostapp.com/main_info.php";	//main_info.php
 		return false;
 	});
 	
 	$("#id4").click(function(){
-		window.location = "contacts.php";
+		window.location = "https://brainstorming.000webhostapp.com/contacts.php";	//contacts.php
 		return false;
 	});
 });
